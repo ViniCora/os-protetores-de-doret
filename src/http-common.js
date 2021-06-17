988d4c8151c070df89bba417be05ba1f9ef0d26c
Vinicius Coradassi <viniciuscoradassi@hotmail.com>
2021-06-17 13:52:06 -0300

    overlay

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  //baseURL: 'https://os-protetores-de-doret-api.herokuapp.com/',
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'https://os-protetores-de-doret-api.herokuapp.com/',
+  //baseURL: 'http://localhost:8080/',
   headers: {
     'Content-type': 'application/json',
   },
